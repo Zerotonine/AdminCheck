6ca5f0c7321a3fa07d7b120be23803bd2041e91e
Zerotonine <52973359+Zerotonine@users.noreply.github.com>
2021-02-08 09:07:56 +0100

    init

---
+++ IsAdmin/IsAdmin/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsAdmin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(admin())
+                Console.WriteLine("Herzlich Willkommen Chef!");
+            else
+                Console.WriteLine("Ey du nix Admin! Du kommst hier net rein!");
+
+
+            Console.ReadKey();
+        }
+
+        static bool admin()
+        {
+            WindowsPrincipal wp = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            return wp.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+}
